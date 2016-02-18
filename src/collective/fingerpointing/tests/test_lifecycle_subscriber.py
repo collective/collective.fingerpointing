34a32e64e677a9ad0e52437e5f3e111314c22b03
hvelarde <hector.velarde@gmail.com>
2016-02-18 15:15:33 -0200

    Fix test fixture for Plone 5

--- src/collective/fingerpointing/tests/test_lifecycle_subscriber.py
+++ src/collective/fingerpointing/tests/test_lifecycle_subscriber.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+"""Tests for lifecycle subscriber.
+
+Events are slightly different among Plone 4 and Plone 5. Also, Plone 5
+content types have different names.
+"""
 from collective.fingerpointing.config import PROJECTNAME
 from collective.fingerpointing.testing import INTEGRATION_TESTING
+from collective.fingerpointing.testing import PLONE_VERSION
 from logging import INFO
 from plone import api
 from testfixtures import LogCapture
@@ -20,22 +26,38 @@ class LifeCycleSubscribersTestCase(unittest.TestCase):
             self.folder = api.content.create(self.portal, 'Folder', 'folder')
 
     def test_object_created(self):
-        with LogCapture(level=INFO) as log:
-            api.content.create(self.folder, 'Folder', 'foo')
-            log.check(
+        if PLONE_VERSION >= '5.0':
+            expected = (
+                ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object created object=<Folder at foo>'),
+                ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object modified object=<Folder at folder>'),
+            )
+        else:
+            expected = (
                 ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object created object=<ATFolder at foo>'),
                 ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object modified object=<ATFolder at folder>'),
                 ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object modified object=<ATFolder at foo>'),
             )
 
-    def test_object_removed(self):
-        api.content.create(self.folder, 'Folder', 'foo')
         with LogCapture(level=INFO) as log:
-            api.content.delete(self.folder['foo'])
-            log.check(
+            api.content.create(self.folder, 'Folder', 'foo')
+            log.check(*expected)
+
+    def test_object_removed(self):
+        if PLONE_VERSION >= '5.0':
+            expected = (
+                ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object removed object=<Folder at foo>'),
+                ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object modified object=<Folder at folder>'),
+            )
+        else:
+            expected = (
                 ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object removed object=<ATFolder at foo>'),
                 ('collective.fingerpointing', 'INFO', 'user=test ip=127.0.0.1 action=object modified object=<ATFolder at folder>'),
             )
+
+        api.content.create(self.folder, 'Folder', 'foo')
+        with LogCapture(level=INFO) as log:
+            api.content.delete(self.folder['foo'])
+            log.check(*expected)
 
     def test_susbcriber_ignored_when_package_not_installed(self):
         # content type life cycle events should not raise errors
