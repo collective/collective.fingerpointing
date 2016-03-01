344298f9cb7c58ef33556f294bce2fbcd5708750
hvelarde <hector.velarde@gmail.com>
2016-03-01 16:59:43 -0300

    Add tests for permissions and user actions

--- src/collective/fingerpointing/tests/test_setup.py
+++ src/collective/fingerpointing/tests/test_setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Ensure add-on is properly installed and uninstalled."""
 from collective.fingerpointing.config import PROJECTNAME
 from collective.fingerpointing.interfaces import IBrowserLayer
 from collective.fingerpointing.testing import INTEGRATION_TESTING
@@ -23,6 +24,23 @@ class InstallTestCase(unittest.TestCase):
     def test_addon_layer(self):
         self.assertIn(IBrowserLayer, registered_layers())
 
+    def test_user_action(self):
+        user_actions = self.portal['portal_actions'].user
+        self.assertIn('audit-log', user_actions)
+
+        permissions = user_actions['audit-log'].permissions
+        expected = ('collective.fingerpointing: Show the Log',)
+        self.assertEqual(permissions, expected)
+        url_expr = user_actions['audit-log'].url_expr
+        self.assertIn('/@@fingerpointing-log', url_expr)
+
+    def test_setup_permission(self):
+        permission = 'collective.fingerpointing: Show the Log'
+        roles = self.portal.rolesOfPermission(permission)
+        roles = [r['name'] for r in roles if r['selected']]
+        expected = ['Manager', 'Site Administrator']
+        self.assertListEqual(roles, expected)
+
 
 class UninstallTestCase(unittest.TestCase):
 
@@ -40,3 +58,7 @@ class UninstallTestCase(unittest.TestCase):
 
     def test_addon_layer_removed(self):
         self.assertNotIn(IBrowserLayer, registered_layers())
+
+    def test_user_action_removed(self):
+        user_actions = self.portal['portal_actions'].user
+        self.assertNotIn('audit-log', user_actions)
