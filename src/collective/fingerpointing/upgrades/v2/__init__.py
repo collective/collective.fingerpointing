de12b0deeefe03ad812c3c07fb9b2746e3c9f269
hvelarde <hector.velarde@gmail.com>
2016-03-07 15:52:10 -0300

    Refactor upgrade step

--- src/collective/fingerpointing/upgrades/v2/__init__.py
+++ src/collective/fingerpointing/upgrades/v2/__init__.py
@@ -8,3 +8,12 @@ def update_configlet(setup_tool):
     profile = 'profile-{0}:default'.format(PROJECTNAME)
     setup_tool.runImportStepFromProfile(profile, 'controlpanel')
     logger.info('Control panel configlet updated.')
+
+
+def update_user_actions(setup_tool):
+    """Update user actions."""
+    profile = 'profile-{0}:default'.format(PROJECTNAME)
+    setup_tool.runImportStepFromProfile(profile, 'rolemap')
+    logger.info('Permissions updated.')
+    setup_tool.runImportStepFromProfile(profile, 'actions')
+    logger.info('User actions updated.')
