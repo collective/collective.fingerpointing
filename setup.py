ae6f1493e651b23eba8e01ec26a753fb4dc94334
hvelarde <hector.velarde@gmail.com>
2016-08-12 17:13:40 -0300

    Add classifier for Plone 5.1
    Refs. https://community.plone.org/t/plone-5-1-classifier-available-for-setup-py/2595?u=hvelarde

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Plone :: 4.3',
         'Framework :: Plone :: 5.0',
-        # 'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.1',
         'Framework :: Plone',
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
