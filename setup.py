aab8c3649bb032435864d0c8a7a60190eac117ca
hvelarde <hector.velarde@gmail.com>
2019-11-22 16:44:41 -0600

    Back to development: 1.8.2
    [skip ci]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '1.8.1'
+version = '1.8.2.dev0'
 description = 'Keep track of different events and write them down to an audit log.'
 long_description = (
     open('README.rst').read() + '\n' +
